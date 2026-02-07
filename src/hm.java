6fd0e5df1d4ab770a7e48f7e824a25cf669204ab
The-Dark-Hydra <rahil.yahoo@gmail.com>
2026-02-08 02:34:27 +0530

    got the working of a loop

--- src/hm.java
+++ src/hm.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class hm {
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("enter a number");
-        int n=scan.nextInt();
+        System.out.print("enter a number: ");
+        int n = scan.nextInt();
 
-
-
-        for(i>>n,i++);
-            System.out.println();
+        for (int i = 0; i < n; i++) {
+            System.out.println("This is a test");
+        }
+    scan.close();
     }
 }
